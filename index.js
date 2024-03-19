ec4ea6fda0559cf6a4fc428ec20f63e5ae7e3082
Ethiopia12345 <101978426+mijaleta@users.noreply.github.com>
2024-03-19 11:54:21 +0300

    all

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const express = require('express');
 const { request } = require('http');
 const mongoose = require('mongoose');
 const path = require('path');
-const Users = require('./models/user')
+const session = require('express-session');
+const flash = require('connect-flash');
+const passport = require('passport');
+
+const User = require('./models/user')
 const indexRouter = require('./routes/index')
+const LocalStrategy = require('passport-local').Strategy;
+
 // Initialize Express app
 const app = express();
-
 // app.use('/users', usersRouter);
-
-
 // Set up static files serving from the public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -23,6 +26,56 @@ app.set('views', [
   ]);
 app.set('view engine', 'ejs'); // Assuming you're using EJS as the template engine
 app.use('/', indexRouter);
+
+
+
+app.use(express.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// Session middleware
+app.use(session({
+  secret: 'r8q,+&1LM3)CD*zAGpx1xm{NeQhc;#',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60 * 60 * 1000 }, // 1 hour
+}));
+// Configure flash middleware
+app.use(flash());
+// Initialize Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+// Define local strategy
+passport.use(new LocalStrategy(
+  async function (username, password, done) {
+      try {
+          const user = await User.findOne({ username });
+          if (!user) {
+              return done(null, false, { message: 'Incorrect username.' });
+          }
+          const isPasswordValid = await bcrypt.compare(password, user.password);
+          if (!isPasswordValid) {
+              return done(null, false, { message: 'Incorrect password.' });
+          }
+          return done(null, user);
+      } catch (error) {
+          return done(error);
+      }
+  }
+));
+// Serialize and deserialize user
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async function (id, done) {
+  try {
+      const user = await User.findById(id);
+      done(null, user);
+  } catch (error) {
+      done(error);
+  }
+});
 
 // for authentication
 
