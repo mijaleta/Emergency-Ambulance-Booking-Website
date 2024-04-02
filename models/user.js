161fa236fdad0a6c723473fa7a147e4d3fbf3aa2
Ethiopia12345 <101978426+mijaleta@users.noreply.github.com>
2024-04-02 11:42:26 +0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1/ambulance-websiteM', {
+mongoose.connect('mongodb://127.0.0.1/ambulance-website', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
