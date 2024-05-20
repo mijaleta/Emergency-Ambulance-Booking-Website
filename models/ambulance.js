239fd496a983c8f5f10a2eed367b1dd79b0ec564
Ethiopia12345 <101978426+mijaleta@users.noreply.github.com>
2024-05-20 09:57:30 +0300

    x

--- models/ambulance.js
+++ models/ambulance.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const ambulanceSchema = new mongoose.Schema({
   type: { type: String, required: true },
   available: { type: Number, required: true },
-  archived: { type: Boolean, default: false } // Default value is false
+  archived: { type: Boolean, default: false }, // Default value is false
+  status: { type: Boolean, default: false } ,// New field for archiving
+
 });
 module.exports = mongoose.model('Ambulance', ambulanceSchema);
